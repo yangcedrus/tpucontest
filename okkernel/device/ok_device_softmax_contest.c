6535cf75cf81b4bed80b5191ccde2c0cdb62ba79
yangcedrus <yangcedrus@gmail.com>
2022-02-17 20:09:45 +0800

    softmax all pass

--- okkernel/device/ok_device_softmax_contest.c
+++ okkernel/device/ok_device_softmax_contest.c
@@ -15,12 +15,13 @@ typedef struct {
 } __attribute__((packed)) param_t;
 
 void conduct_softmax(local_addr_t input_addr, local_addr_t exp_addr, local_addr_t output_addr, dim4 shape, dim4 stride){
+    (void)stride;
     // TODO handle overflow
     // get max value
     // sub max value
 
     // sub constant 10
-    okk_bdc_sub_C(input_addr, input_addr, 10.0, &shape, &stride, &stride);
+    okk_bdc_sub_C(input_addr, input_addr, 5.0, &shape, &stride, &stride);
 
     // cal exp(X)
     okk_bdc_exp(exp_addr, input_addr, output_addr, &shape);
@@ -30,24 +31,15 @@ void conduct_softmax(local_addr_t input_addr, local_addr_t exp_addr, local_addr_
     dim4 kernel_shape = {.n=1, .c=64, .h=1, .w=2};
     dim4 kernel_stride;
     okk_compact_stride(&kernel_stride, 0, &kernel_shape);
-    // OKKERNEL_LOG("kernel shape is [%d,%d,%d,%d]", kernel_shape.n, kernel_shape.c, kernel_shape.h, kernel_shape.w);
-    // OKKERNEL_LOG("kernel stride is [%d,%d,%d,%d]", kernel_stride.n, kernel_stride.c, kernel_stride.h, kernel_stride.w);
-    // OKKERNEL_LOG("input addr:%d, exp addr:%d, output addr:%d, total size:%d, mem size:%d", input_addr, exp_addr, output_addr, output_addr + kernel_shape.n * kernel_stride.n * sizeof(float), LOCAL_MEM_SIZE);
+
     OKKERNEL_ASSERT(output_addr + kernel_shape.n * kernel_stride.n * sizeof(float) < LOCAL_MEM_SIZE);
     okk_bdc_32bit_set_C(output_addr, C, &kernel_shape, &kernel_stride);
 
-    // dim4 kernel_shape_2IC = {.n = (shape.c + 1) / 2, .c = shape.c, .h = 1, .w = 1};
-    // dim4 kernel_stride_2IC;
-    // okk_compact_stride(&kernel_stride_2IC, 0, &kernel_shape_2IC);
     dim4 kernel_stride_2IC = {.n=0,.c=0,.h=0,.w=0};
-    // OKKERNEL_LOG("kernel shape is [%d,%d,%d,%d]", kernel_shape_2IC.n, kernel_shape_2IC.c, kernel_shape_2IC.h, kernel_shape_2IC.w);
-    // OKKERNEL_LOG("kernel stride is [%d,%d,%d,%d]", kernel_stride_2IC.n, kernel_stride_2IC.c, kernel_stride_2IC.h, kernel_stride_2IC.w);
 
     okk_bdc_conv2d(input_addr, exp_addr, output_addr, NULL, &shape, shape.c, 1, 1, &stride, &kernel_stride_2IC, false, false, NULL, NULL, NULL);
 
     // div exp(x) by sum
-    // OKKERNEL_LOG("stride is [%d,%d,%d,%d]", stride.n, stride.c, stride.h, stride.w);
-    // OKKERNEL_LOG("broadcast_stride is [%d,%d,%d,%d]", broadcast_stride.n, broadcast_stride.c, broadcast_stride.h, broadcast_stride.w);
     okk_bdc_div(output_addr, exp_addr, input_addr, &shape, &stride, &stride, &stride);
 }
 
@@ -63,9 +55,6 @@ void softmax_contest(const void *args) {
     exp_addr = input_addr + shape.n * stride.n * sizeof(float);
     output_addr = exp_addr + shape.n * stride.n * sizeof(float);
 
-    // OKKERNEL_LOG("input addr:%d, exp addr:%d, output addr:%d, total size:%d, mem size:%d", input_addr, exp_addr, output_addr, output_addr + shape.n * stride.n * sizeof(float), LOCAL_MEM_SIZE);
-
-    // OKKERNEL_ASSERT(output_addr + shape.n * stride.n * sizeof(float) <= LOCAL_MEM_SIZE);
     if (output_addr + shape.n * stride.n * sizeof(float) <= LOCAL_MEM_SIZE)
     {
         // common situation
@@ -81,7 +70,6 @@ void softmax_contest(const void *args) {
         {
             // OKKERNEL_LOG("data can be split by N");
             int group_size = LOCAL_MEM_SIZE / (3 * stride.n * sizeof(float));
-            // OKKERNEL_LOG("group size:%d", group_size);
             int temp_num = 0;
             int tensor_size = param->C * param->H * param->W * sizeof(float);
             while(temp_num < param->N)
@@ -119,22 +107,52 @@ void softmax_contest(const void *args) {
 
             if(LOCAL_MEM_SIZE / (3*new_shape.n * new_stride.n * sizeof(float)) != 0)
             {
+                // TODO
                 // split data by HW
-                OKKERNEL_LOG("data can be split by HW");
+                // OKKERNEL_LOG("data can be split by HW");
             }else
             {
                 // split data by N & HW
                 // TODO catch error for too big tensor
-                OKKERNEL_LOG("data can be split by N & HW");
+                // OKKERNEL_LOG("data can be split by N & HW");
                 int group_size = LOCAL_MEM_SIZE / (3 * new_stride.n * sizeof(float));
-                // OKKERNEL_LOG("group size:%d", group_size);
                 int temp_num = 0;
                 int tensor_size = new_shape.c * new_shape.h * new_shape.w * sizeof(float);
                 for(int i = 0; i < 4; i++)
                 {
-                    int sys_offset = (i/2)*((param->H + 1)/2) * param->N * param->C * param->W * sizeof(float) + (i%2) * ((param->W + 1)/2) * sizeof(float);
+                    int sys_offset = (i/2)*((param->H + 1)/2) * param->W * sizeof(float) + (i%2) * ((param->W + 1)/2) * sizeof(float);
                     dim4 sys_stride = {.n=param->C * param->H * param->W, .c=param->H * param->W, .h=param->W, .w=1};
 
+                    int group_size = LOCAL_MEM_SIZE / (3 * new_stride.n * sizeof(float));
+                    int temp_num = 0;
+                    int tensor_size = shape.c * shape.h * shape.w * sizeof(float);
+                    while(temp_num < param->N)
+                    {
+                        if(temp_num + group_size <= param->N)
+                            new_shape.n = group_size;
+                        else
+                            new_shape.n = param->N - temp_num;
+
+                        input_addr = 0;
+                        exp_addr = input_addr + new_shape.n * new_stride.n * sizeof(float);
+                        output_addr = exp_addr + new_shape.n * new_stride.n * sizeof(float);
+
+                        int N_system_offset = sys_offset + temp_num * tensor_size;
+
+                        okk_gdma_32bit_cpy_S2L(input_addr, param->input_addr + N_system_offset, &new_shape, NULL, &sys_stride);
+
+                        conduct_softmax(input_addr, exp_addr, output_addr, new_shape, new_stride);
+
+                        okk_gdma_32bit_cpy_L2S(param->output_addr + N_system_offset, output_addr, &new_shape, &sys_stride, NULL);
+
+                        temp_num += group_size;
+                    }
+                    
+                    new_shape.w = param->W - new_shape.w;
+                    if(i==1)
+                        new_shape.h = param->H - new_shape.h;
+
+                    okk_128_byte_aligned_stride_for_32bit(&new_stride, 0, &new_shape);
                 }
             }
         }
